ec2de3452f945a6e55442e8c281994d003f89ef7
wenjian <1787176370@qq.com>
2018-04-23 14:32:33 +0800

    Update weibologin.py

--- app_v1/api/weibologin.py
+++ app_v1/api/weibologin.py
@@ -7,7 +7,7 @@ import requests, json
 @api.route("/getInfo")
 def get_access_token():
 	code = request.query_string.decode().split('=')[1]
-	url  = "https://api.weibo.com/oauth2/access_token?client_id=1981969326&client_secret=bdb57632f9faf0a74d9771ce554b34e3&grant_type=authorization_code&redirect_uri=http%3a%2f%2fwww.wenjian.group&code="+code
+	url  = "https://api.weibo.com/oauth2/access_token?client_id=1981969326&client_secret=xxx&grant_type=authorization_code&redirect_uri=http%3a%2f%2fwww.wenjian.group&code="+code
 	postData = {}
 	try:
 		response = requests.post(url, data=json.dumps(postData))
@@ -16,9 +16,9 @@ def get_access_token():
 			access_token = json.loads(response.text)['access_token']
 			return jsonify({"result":True, "access_token":access_token})
 		else:
-			return jsonify({"result":False, "message":"请求失败1"})
+			return jsonify({"result":False, "message":"请求失败"})
 	except requests.RequestException as e:
-		return jsonify({"result":False, "message":"请求失败2"})
+		return jsonify({"result":False, "message":"请求失败"})
 
 
 
